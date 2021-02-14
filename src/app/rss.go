a2f17379852eebfc4071b7efdda297fb39e2c45f
Nail <kouta@list.ru>
2021-02-14 18:33:56 +0100

    Add tokenizer.

--- src/app/rss.go
+++ src/app/rss.go
@@ -2,19 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/jdkato/prose"
 	"github.com/mmcdole/gofeed"
 )
 
 func main() {
-	// file, _ := os.Open("/home/user/Downloads/rss")
+	file, _ := os.Open("/home/user/Downloads/rss")
 	fp := gofeed.NewParser()
-	feed, _ := fp.ParseURL("https://www.reddit.com/r/science/.rss")
-	// feed, _ := fp.Parse(file)
+	// feed, _ := fp.ParseURL("https://www.reddit.com/r/science/.rss")
+	feed, _ := fp.Parse(file)
 	var result []string
 	for _, it := range feed.Items {
 		result = append(result, it.Title)
-		// result = append(result, it.Content)
+		tokens := createTokens(it.Content)
+		fmt.Print(tokens)
+		fmt.Print("\n----------------------\n")
 	}
-	fmt.Printf("%v", result)
+
+	// fmt.Printf("%v", result)
+}
+
+func createTokens(text string) []string {
+	doc, err := prose.NewDocument(text)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var textTokens []string
+	for _, tok := range doc.Tokens() {
+		textTokens = append(textTokens, tok.Text)
+		fmt.Print("[" + tok.Text + "]\n")
+	}
+	return textTokens
 }
