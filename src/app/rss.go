4aaaa9728ac7944764f14a97db3aeac8426ed4a8
Nail <kouta@list.ru>
2021-12-06 20:13:15 +0100

    Return rss parser, create csv out of it.

--- src/app/rss.go
+++ src/app/rss.go
@@ -1,28 +1,37 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jdkato/prose"
 	"github.com/mmcdole/gofeed"
 )
 
 func main() {
-	file, _ := os.Open("/home/user/Downloads/rss")
+	file, _ := os.Open("/home/user/Downloads/science.xml")
+	csvFile, err := os.OpenFile("texts.csv", os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	csvwriter := csv.NewWriter(csvFile)
 	fp := gofeed.NewParser()
 	// feed, _ := fp.ParseURL("https://www.reddit.com/r/science/.rss")
 	feed, _ := fp.Parse(file)
-	var result []string
-	for _, it := range feed.Items {
-		result = append(result, it.Title)
-		tokens := createTokens(it.Content)
-		fmt.Print(tokens)
-		fmt.Print("\n----------------------\n")
-	}
 
-	// fmt.Printf("%v", result)
+	for _, it := range feed.Items {
+		if it.Title != "" {
+			var moreReplaced = strings.Replace(it.Description, " <!-- more -->", "", 1)
+			var row = []string{it.Title + "\n" + moreReplaced, "0"}
+			_ = csvwriter.Write(row)
+		}
+	}
+	csvwriter.Flush()
+	csvFile.Close()
 }
 
 func createTokens(text string) []string {
